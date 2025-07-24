ef90523e6e969e53cfbd7c9076d59734731bc325
Khang Vu <49794277+khangvu0@users.noreply.github.com>
2025-07-24 16:59:38 -0400

    Created calculator object and made click events for the buttons. Still need to create the functions within the calculator object.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const num = document.querySelectorAll('num');
-const operand = document.querySelectorAll('operand');
+const num = document.querySelectorAll('.num');
+const operand = document.querySelectorAll('.operand');
 const clear = document.getElementById('clear');
 const backspace = document.getElementById('backspace');
 const percent = document.getElementById('percent');
@@ -9,6 +9,50 @@ const dot = document.getElementById('dot');
 const equal = document.getElementById('equal');
 
 const calculator = {
-    
+    displayValue: '0',
+
+    append(char) {
+
+    },
+
+    updateDisplay() {
+
+    },
+
+    backspace() {
+
+    },
+
+    clear(){
+
+    },
+
+    percent() {
+
+    },
+
+    compute() {
+
+    }
 };
 
+// Click event for each number button 
+num.forEach(btn =>
+    btn.addEventListener('click', () => {
+        calculator.append(btn.textContent);
+    })
+);
+
+// Click event for each operand button
+operand.forEach(btn =>
+    btn.addEventListener('click', () => {
+        calculator.append(btn.textContent);
+    })
+);
+
+// Click event for other buttons
+clear.addEventListener('click', () => calculator.clear());
+backspace.addEventListener('click', () => calculator.backspace());
+dot.addEventListener('click', () => calculator.append('.'));
+equal.addEventListener('click', () => calculator.calculate());
+percent.addEventListener('click', () => calculator.makePercent());
