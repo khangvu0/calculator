3d1b587452edc1d1745fdb083b30524781f8f52e
Khang Vu <49794277+khangvu0@users.noreply.github.com>
2025-07-24 15:23:11 -0400

    Declared DOM variables

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const num = document.querySelectorAll('num');
+const operand = document.querySelectorAll('operand');
+const clear = document.getElementById('clear');
+const backspace = document.getElementById('backspace');
+const percent = document.getElementById('percent');
+const dot = document.getElementById('dot');
+const equal = document.getElementById('equal');
+
+const calculator = {
+    
+};
+
