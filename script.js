b320b892c6ee99826028c99cb555d8c1c2bd81b9
Khang Vu <49794277+khangvu0@users.noreply.github.com>
2025-07-24 21:34:22 -0400

    Updated script.js, made append and updateDisplay functions

--- script.js
+++ script.js
@@ -7,20 +7,35 @@ const backspace = document.getElementById('backspace');
 const percent = document.getElementById('percent');
 const dot = document.getElementById('dot');
 const equal = document.getElementById('equal');
+const display = document.getElementById('display')
 
 const calculator = {
     displayValue: '0',
+    oldValue: '',
 
     append(char) {
+        // Checks for 2 periods in the display
+        if (char === '.' && this.displayValue.includes('.')) {
+            alert('Can not have 2 decimals.');
+            return;
+        }
 
+        // If display is zero, set display to char. Else append char
+        if (this.displayValue === '0' && char !== '.'){
+            this.displayValue = char;
+        } else {
+            this.displayValue += char;
+        }
+
+        this.updateDisplay()
     },
 
-    updateDisplay() {
-
+    updateDisplay() {       
+        display.textContent = this.displayValue;
     },
 
     backspace() {
-
+        
     },
 
     clear(){
@@ -44,6 +59,7 @@ num.forEach(btn =>
 );
 
 // Click event for each operand button
+//foreach is a shorthand for a forloop
 operand.forEach(btn =>
     btn.addEventListener('click', () => {
         calculator.append(btn.textContent);
@@ -51,8 +67,8 @@ operand.forEach(btn =>
 );
 
 // Click event for other buttons
-clear.addEventListener('click', () => calculator.clear());
-backspace.addEventListener('click', () => calculator.backspace());
+clear.addEventListener('click', calculator.clear);
+backspace.addEventListener('click', calculator.backspace);
 dot.addEventListener('click', () => calculator.append('.'));
-equal.addEventListener('click', () => calculator.calculate());
-percent.addEventListener('click', () => calculator.makePercent());
+equal.addEventListener('click', calculator.calculate);
+percent.addEventListener('click', calculator.makePercent);
